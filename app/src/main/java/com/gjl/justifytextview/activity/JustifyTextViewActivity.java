667b1aa87cb9588965794399a3cc040c9ae2ec57
guojilong <guo245296059@gmail.com>
2016-03-16 17:05:23 +0800

    use databinding

--- app/src/main/java/com/gjl/justifytextview/activity/JustifyTextViewActivity.java
+++ app/src/main/java/com/gjl/justifytextview/activity/JustifyTextViewActivity.java
@@ -1,9 +1,12 @@
 package com.gjl.justifytextview.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.gjl.justifytextview.R;
+import com.gjl.justifytextview.databinding.ActivityJustifytextviewBinding;
 
 /**
  * Created by guojilong on 16/3/15.
@@ -14,6 +17,14 @@ public class JustifyTextViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_justifytextview);
+        final ActivityJustifytextviewBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_justifytextview);
+        binding.ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                binding.normalTv.setText(binding.edit.getText().toString() + "");
+                binding.justfyTv.setText(binding.edit.getText().toString() + "");
+            }
+        });
     }
 }
